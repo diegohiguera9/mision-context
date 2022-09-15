6d227a3b889682139ecbeba888cf87ddf5e54fc1
Ruben Dario Castilla Romero <67343176+CastRome@users.noreply.github.com>
2022-09-15 15:12:55 -0500

    tercer commit

--- src/Radio.js
+++ src/Radio.js
@@ -8,7 +8,7 @@ const Radio = () => {
     Ruben: false,
     Jhon: false,
     Sergio: false,
-    Todos: false,
+    Todos: true,
   });
 
   const Count = useContext(CountContext);
@@ -17,6 +17,21 @@ const Radio = () => {
     Count.handleOption(option);
     Count.handleCandidato(candidato);
   }, [option, candidato]);
+
+  const handleClick = (who, check) => {
+    console.log(`who:${who}, ev:${check}`)
+    if (who === "Todos") {
+      setCandidato({
+        Diego: false,
+        Ruben: false,
+        Jhon: false,
+        Sergio: false,
+        Todos: true,
+      })
+    } else {
+      setCandidato({ ...candidato, [who]: check, ["Todos"]: false })
+    }
+  }
 
   return (
     <div className="sectionsFlex">
@@ -43,71 +58,76 @@ const Radio = () => {
           />
         </div>
       </div>
-    <div className="candidatosFlex">
-      <span> Escoge candidato</span>
-      <div>
-        <label htmlFor="Diego">Diego</label>
-        <input
-          type="checkbox"
-          id="Diego"
-          name="radio2"
-          value="Diego"
-          onChange={(e) =>
-            setCandidato({ ...candidato, ["Diego"]: e.target.checked })
-          }
-        />
+      <div className="candidatosFlex">
+        <span> Escoge candidato</span>
+        <div>
+          <label htmlFor="Diego">Diego</label>
+          <input
+            type="checkbox"
+            id="Diego"
+            name="radio2"
+            value="Diego"
+            checked={candidato.Diego}
+            onChange={(e) =>
+              handleClick("Diego", e.target.checked)
+            }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="Ruben">Ruben</label>
+          <input
+            type="checkbox"
+            id="Ruben"
+            name="radio2"
+            value="Ruben"
+            checked={candidato.Ruben}
+            onChange={(e) =>
+              handleClick("Ruben", e.target.checked)
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="John">John</label>
+          <input
+            type="checkbox"
+            id="John"
+            name="radio2"
+            value="Jhon"
+            checked={candidato.Jhon}
+            onChange={(e) =>
+              handleClick("Jhon", e.target.checked)
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="Sergio">Sergio</label>
+          <input
+            type="checkbox"
+            id="Sergio"
+            name="radio2"
+            value="Sergio"
+            checked={candidato.Sergio}
+            onChange={(e) =>
+              handleClick("Sergio", e.target.checked)
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="Todos">Todos</label>
+          <input
+            type="checkbox"
+            id="Todos"
+            name="radio2"
+            value="Todos"
+            checked={candidato.Todos}
+            onChange={(e) =>
+              handleClick("Todos", e.target.checked)
+            }
+          />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor="Ruben">Ruben</label>
-        <input
-          type="checkbox"
-          id="Ruben"
-          name="radio2"
-          value="Ruben"
-          onChange={(e) =>
-            setCandidato({ ...candidato, ["Ruben"]: e.target.checked })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="John">John</label>
-        <input
-          type="checkbox"
-          id="John"
-          name="radio2"
-          value="Jhon"
-          onChange={(e) =>
-            setCandidato({ ...candidato, ["Jhon"]: e.target.checked })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="Sergio">Sergio</label>
-        <input
-          type="checkbox"
-          id="Sergio"
-          name="radio2"
-          value="Sergio"
-          onChange={(e) =>
-            setCandidato({ ...candidato, ["Sergio"]: e.target.checked })
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="Todos">Todos</label>
-        <input
-          type="checkbox"
-          id="Todos"
-          name="radio2"
-          value="Todos"
-          onChange={(e) =>
-            setCandidato({ ...candidato, ["Todos"]: e.target.checked })
-          }
-        />
-      </div>  
-    </div>
-      
     </div>
   );
 };
